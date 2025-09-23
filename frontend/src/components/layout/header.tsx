96be241f5bf30a091426497df4ceb0637d0d3010
jvcbyte <jvc8463@gmail.com>
2025-09-23 15:17:06 +0100

    update header

--- frontend/src/components/layout/header.tsx
+++ frontend/src/components/layout/header.tsx
@@ -32,7 +32,7 @@ export const HeroHeader = () => {
             "bg-background/75 border-b border-black/5 backdrop-blur-lg",
         )}
       >
-        <div className="mx-auto max-w-5xl px-6">
+        <div className="mx-auto max-w-7xl px-6">
           <div className="relative flex flex-wrap items-center justify-between gap-6 py-3 lg:gap-0">
             <div className="flex w-full justify-between gap-6 lg:w-auto">
               <Link
